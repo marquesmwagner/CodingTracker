df8285b1b293ca1b43681322b3242f82f4b8de90
marquesmwagner <marquesmwagner@gmail.com>
2022-12-20 11:34:19 -0300

    Add properties in CodingSession Class.

--- CodingTracker/Models/CodingSession.cs
+++ CodingTracker/Models/CodingSession.cs
@@ -8,5 +8,10 @@ namespace CodingTracker.Models
 {
     internal class CodingSession
     {
+        public int Id { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public DateTime Duration { get; set; }
+        
     }
 }
