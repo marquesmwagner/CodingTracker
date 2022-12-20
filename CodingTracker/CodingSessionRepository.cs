cf305eaf8e344976167080ccbd931519882eb0f2
marquesmwagner <marquesmwagner@gmail.com>
2022-12-20 15:19:09 -0300

    Add CreateDatabase method.

--- CodingTracker/CodingSessionRepository.cs
+++ CodingTracker/CodingSessionRepository.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SQLite;
 
 namespace CodingTracker
 {
     internal class CodingSessionRepository
     {
+        static string connectionString = "coding-tracker.db";
+
+        internal static void CreateDatabase()
+        {
+            SQLiteConnection.CreateFile(connectionString);
+        }
     }
 }
