b013a93409faf0c4b7455ee34e2387414d61940d
marquesmwagner <marquesmwagner@gmail.com>
2022-12-21 12:45:31 -0300

    Add GetInput method.

--- CodingTracker/Helpers.cs
+++ CodingTracker/Helpers.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,23 @@ namespace CodingTracker
 {
     internal class Helpers
     {
+        internal static string GetInput(string message)
+        {
+            var provider = CultureInfo.InvariantCulture;
+
+            Console.WriteLine(message);
+            var input = Console.ReadLine();
+
+            if (input == "0") return input;
+
+            while (!DateTime.TryParseExact(input, "dd-MM-yy HH:mm", provider, DateTimeStyles.None, out _))
+            {
+                Console.WriteLine("\nInvalid input (Format: dd-mm-yy hh:mm). Type 0 to go back to menu or try again.");
+                input = Console.ReadLine();
+            }    
+
+            return input;
+
+        }
     }
 }
