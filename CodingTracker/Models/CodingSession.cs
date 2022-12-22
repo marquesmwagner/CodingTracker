e6dd2baf74fc98e8acf93c9855f3b75f9871176a
marquesmwagner <marquesmwagner@gmail.com>
2022-12-22 14:57:38 -0300

    Redefine Duration data type to TimeSpan.

--- CodingTracker/Models/CodingSession.cs
+++ CodingTracker/Models/CodingSession.cs
@@ -11,7 +11,7 @@ namespace CodingTracker.Models
         public int Id { get; set; }
         public DateTime StartTime { get; set; }
         public DateTime EndTime { get; set; }
-        public DateTime Duration { get; set; }
+        public TimeSpan Duration { get; set; }
         
     }
 }
